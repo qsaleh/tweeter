422be15ab19f28d2286e2f1e718db0cb633edf64
qsaleh <qmsaleh@gmail.com>
2020-02-01 19:02:20 +0000

    final commit after completion of de-bugging

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,42 +5,26 @@
  */
 
 jQuery(function($) {
-  const tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
 
 const $tweetContainer = $(".tweet-container");
 
 const renderTweets = function(tweets) {
 
-
+  $tweetContainer.empty();
+  tweets.reverse();
   const $createdTweets = $(tweets.map(createTweetElement).join(" "));
   return $tweetContainer.append($createdTweets);
 }
 
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
+
 const createTweetElement = function(tweet) {
+
+  const safeHTML = escape(tweet.content.text);
 
   const dateToday = Date.now();
   const datePosted = tweet.created_at;
@@ -50,13 +34,57 @@ const createTweetElement = function(tweet) {
 `
   <article class="tweet">
         <div class="tweet-header"> 
-          <p class="user-name"><img src=${tweet.user.avatars}>${tweet.user.name}</p>
+          <p class="user-name"><img src=${tweet.user.avatars} height="60" width="60">${tweet.user.name}</p>
           <p class="handle">${tweet.user.handle}</p></div>
-          <p class="tweet-text">${tweet.content.text}</p>
+          <p class="tweet-text">${safeHTML}</p>
       <div class="tweet-footer">${daysAgo} days ago</div></article> `;
       return markUp;
 
 }
 
-renderTweets(tweetData);
+//renderTweets(tweetData);
+$("#nav-down").click(function() {
+  $([document.documentElement, document.body]).animate({
+      scrollTop: $("#img-align").offset().top
+  }, 1200);
+});
+
+
+$(document).ready(function(){
+
+  $('#target').on('submit', function (event){
+    event.preventDefault();
+    const data = $(this).serialize();
+   if (data.length > 145) {
+    $("#error-message").text("Message length exceeded");
+   } else if (data.length < 6) {
+    $("#error-message").text("Please enter Message"); 
+   } else {
+    
+  $.ajax({
+    url: '/tweets',
+    method: 'POST',
+    data,
+    success: (data) => {
+       
+        loadTweets(data);
+    },
+    error: (error) => {
+        console.log('this request failed and this was the error', error);
+    }
+  });
+  
+  function loadTweets(data) {
+  $.ajax({
+    url: '/tweets',
+    method: 'GET',
+    data,
+    success: (data) => {
+        renderTweets(data);   
+    }
+  })
+  }
+}
+ });
+});
 });
